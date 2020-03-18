319182269bae939df8e0de02ec0b9225d1055698
Asaf Silman <asafsilman@gmail.com>
2020-03-18 17:16:18 +0800

    Added new model train script

---
+++ scripts/TID-RPS-ISO-m3.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#SBATCH -N 1      # nodes requested
+#SBATCH -n 1      # tasks requested
+#SBATCH --mem=4G  # memory in Mb
+#SBATCH --partition=mlgpu
+#SBATCH -o outfile-TID-RPS-ISO-m3  # send stdout to outfile
+#SBATCH -e errfile-TID-RPS-ISO-m3  # send stderr to errfile
+
+cd ~/GalaxyEnvironmentAnalysis
+source env/bin/activate
+python gea.py --debug train config/1.0-TID-RPS-ISO-m3.yml --new-model
