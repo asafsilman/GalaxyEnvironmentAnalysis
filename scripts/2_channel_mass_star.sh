88c17c1458feb08525a796849243a9d8a5a75831
Asaf Silman <asafsilman@gmail.com>
2020-03-19 13:11:57 +0800

    Added two channel model scripts

---
+++ scripts/2_channel_mass_star.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#SBATCH -N 1      # nodes requested
+#SBATCH -n 1      # tasks requested
+#SBATCH --mem=120G  # memory in Mb
+#SBATCH --partition=mlgpu
+#SBATCH -o outfile-2_channel_mass_star  # send stdout to outfile
+#SBATCH -e errfile-2_channel_mass_star  # send stderr to errfile
+
+cd ~/GalaxyEnvironmentAnalysis
+source env/bin/activate
+python gea.py --debug train config/1.0-2_channel_mass_star.yml --new-model
