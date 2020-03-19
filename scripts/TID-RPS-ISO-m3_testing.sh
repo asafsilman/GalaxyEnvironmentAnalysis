134daee9465e87ead3dc1829fb30048ea5b408cf
Asaf Silman <asafsilman@gmail.com>
2020-03-19 11:26:07 +0800

    Added testing script

---
+++ scripts/TID-RPS-ISO-m3_testing.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#SBATCH -N 1      # nodes requested
+#SBATCH -n 1      # tasks requested
+#SBATCH --mem=40G  # memory in Mb
+#SBATCH --partition=mlgpu
+#SBATCH -o outfile-TID-RPS-ISO-m3_1  # send stdout to outfile
+#SBATCH -e errfile-TID-RPS-ISO-m3_1  # send stderr to errfile
+
+cd ~/GalaxyEnvironmentAnalysis
+source env/bin/activate
+python gea.py --debug train config/config_testing.yml --new-model
