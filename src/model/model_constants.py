bf61bf979af8bdea81f9062b80ffc50f512c32fa
Asaf Silman <asafsilman@gmail.com>
2020-05-04 00:33:05 +0800

    Added m4a, m7, m8 and m9 data to model config for RPS, updated erronious data files

--- src/model/model_constants.py
+++ src/model/model_constants.py
@@ -3,7 +3,7 @@ CHANNELS = ["gas_density", "gas_kinematics", "star_density", "star_kinematics"]
 
 MAX_CHANNELS = 4
 
-AVAILABLE_DATASETS = ["m1", "m2", "m3", "m4", "m5", "m6"]
+AVAILABLE_DATASETS = ["m1", "m2", "m3", "m4", "m4a", "m5", "m6", "m7", "m8", "m9"]
 
 DATA_LABELS = {
     "TID": 0,
